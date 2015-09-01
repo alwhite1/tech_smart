1553c1dbae1acfc31f4a035e4396f451305ae328
alwhite1 <alwhite1@rambler.ru>
2015-09-01 17:19:32 +0300

    add new feature to E2E app: use jQuery for detailed month report

--- e2e/urls.py
+++ e2e/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,8 +11,9 @@ urlpatterns = patterns('',
                        url(r'^main/', 'e2e.views.main'),
                        url(r'^report/', 'e2e.views.report'),
                        url(r'^generate_month_report/', 'e2e.views.generate_month_report'),
-                       url(r'^see_post/', 'e2e.views.test_filter_crash'),
+                       url(r'^see_post/', 'e2e.views.see_post'),
                        url(r'^month_report/', 'e2e.views.month_report'),
+                       url(r'^json_report/(?P<month_year>[0-9]+[\.]+[20]+[1-2]+[1-9])', 'e2e.views.month_json'),
                        url(r'(?P<month_year>[0-9]+[\.]+[20]+[1-2]+[1-9])', 'e2e.views.month_detailed'),
                        )
 
